9765798b44d89f914f8526c2a811f55c97d8f311
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-06 14:29:10 +0700

    set product data in widget of Head

--- lib/screens/details/components/detail_body.dart
+++ lib/screens/details/components/detail_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_app/constants.dart';
 import 'package:flutter_shopping_app/models/Product.dart';
 
 class DetailBody extends StatelessWidget {
@@ -24,6 +25,48 @@ class DetailBody extends StatelessWidget {
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(24),
                           topRight: Radius.circular(24))),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Aristrocratic Hand Bag",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        product.title,
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          RichText(
+                              text: TextSpan(children: [
+                            TextSpan(text: "Price\n"),
+                            TextSpan(
+                                text: "\$${product.price}",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline4
+                                    .copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold))
+                          ])),
+                          SizedBox(
+                            width: kDefaultPaddin,
+                          ),
+                          Expanded(
+                              child: Image.asset(
+                            product.image,
+                            fit: BoxFit.fill,
+                          ))
+                        ],
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
