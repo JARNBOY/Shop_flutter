e9a8c4429f70187723d55c7f6a91d2372f78c178
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-06 10:51:43 +0700

    buildCategory

--- lib/screens/home/component/home_body.dart
+++ lib/screens/home/component/home_body.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_app/constants.dart';
 
 class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories()
+      ],
+    );
   }
+}
+
+class Categories extends StatefulWidget {
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["Hand Bag", "Jewelly", "Footwear", "Dresses"];
+  //By default first Item
+  int selectedindex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (context, index) => buildCategory(index)),
+    );
+  }
+
+  Widget buildCategory(int index) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+        child: Text(categories[index]),
+      );
 }
