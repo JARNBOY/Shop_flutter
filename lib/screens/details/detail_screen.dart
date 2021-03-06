4935f50ccd8a0601398d3f5cf9057edb2dfdc25c
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-06 14:01:01 +0700

    add back action

--- lib/screens/details/detail_screen.dart
+++ lib/screens/details/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_app/models/Product.dart';
 import 'package:flutter_shopping_app/screens/details/components/detail_body.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../constants.dart';
 
 class DetailScreen extends StatelessWidget {
   static String routeName = "/detail";
@@ -11,6 +14,8 @@ class DetailScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments;
     print("arguments = prduct.title = ${arguments.product.title}");
     return Scaffold(
+      backgroundColor: arguments.product.color,
+      appBar: buildAppBar(context),
       body: DetailBody(),
     );
   }
@@ -20,4 +25,37 @@ class ProductDetailArguments {
   final Product product;
 
   ProductDetailArguments({@required this.product});
+}
+
+AppBar buildAppBar(BuildContext context) {
+  return AppBar(
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    leading: IconButton(
+      icon: SvgPicture.asset(
+        "assets/icons/back.svg",
+        color: Colors.white,
+      ),
+      onPressed: () => Navigator.pop(context),
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/search.svg",
+          color: Colors.white,
+        ),
+        onPressed: () {},
+      ),
+      IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/cart.svg",
+          color: Colors.white,
+        ),
+        onPressed: () {},
+      ),
+      SizedBox(
+        width: kDefaultPaddin / 2,
+      )
+    ],
+  );
 }
