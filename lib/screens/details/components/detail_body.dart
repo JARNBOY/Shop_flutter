de5c206f24430ba478d0841f9848b45c97a456e9
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-06 14:36:24 +0700

    ProductTitleWithImage

--- lib/screens/details/components/detail_body.dart
+++ lib/screens/details/components/detail_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shopping_app/constants.dart';
 import 'package:flutter_shopping_app/models/Product.dart';
+import 'package:flutter_shopping_app/screens/details/components/product_title_with_image.dart';
 
 class DetailBody extends StatelessWidget {
   final Product product;
@@ -25,49 +25,9 @@ class DetailBody extends StatelessWidget {
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(24),
                           topRight: Radius.circular(24))),
+                  child: Column(),
                 ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Aristrocratic Hand Bag",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        product.title,
-                        style: Theme.of(context).textTheme.headline4.copyWith(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        children: <Widget>[
-                          RichText(
-                              text: TextSpan(children: [
-                            TextSpan(text: "Price\n"),
-                            TextSpan(
-                                text: "\$${product.price}",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headline4
-                                    .copyWith(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold))
-                          ])),
-                          SizedBox(
-                            width: kDefaultPaddin,
-                          ),
-                          Expanded(
-                              child: Image.asset(
-                            product.image,
-                            fit: BoxFit.fill,
-                          ))
-                        ],
-                      )
-                    ],
-                  ),
-                )
+                ProductTitleWithImage(product: product),
               ],
             ),
           )
